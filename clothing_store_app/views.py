bb2046e9b6868a3be4ef9782892fd0d40cb510f2
Joaquin Inthamoussu <joa.inthamoussu@gmail.com>
2024-01-03 21:19:28 +0100

    Style of checkout, Build models, Render products

--- clothing_store_app/views.py
+++ clothing_store_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request, 'store.html', context)
 
 def cart(request):
